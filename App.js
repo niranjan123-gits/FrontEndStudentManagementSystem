30d009b653b782b5170c91f39e5c1e51d15b970b
Niranjan Kumar <leetcodeniru@gmail.com>
2025-10-22 17:19:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import AddUser from './Users/AddUser';
+import EditUser from './Users/EditUser';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+
+     <Navbar/>
+      
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/adduser" element={<AddUser/>}/>
+        <Route exact path="/edituser/:id" element={<EditUser/>}/>
+      </Routes>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
